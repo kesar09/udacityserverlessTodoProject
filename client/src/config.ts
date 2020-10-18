1747cf3db3a95a8b175bc8ef9bab9b3551db6032
Kesar Pandita <kesar@simsol.in>
2020-10-18 18:16:18 +0530

    created a working application which will perforn CRUD operation

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '8cmf0dokr1'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'kesar09.us.auth0.com',           // Auth0 domain
+  clientId: 'T26rAqOFdQm9OW190tj8q50kRxoEbyOR',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
